2c1442e53d8beaf326713e653ca91de49740b120
Raj-1337 <srk1998007@gmail.com>
2020-01-31 11:00:23 +0530

    Updates

---
+++ precache-manifest.1e48b8094b20a6971d0ff7fd08416ef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391735951026d97ad3178be9e9f1c6eb",
+    "url": "/React-Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a69ef9a6fb9cba6fb119",
+    "url": "/React-Monsters-rolodex/static/css/main.8b47ed75.chunk.css"
+  },
+  {
+    "revision": "53b69ea71ede527372a2",
+    "url": "/React-Monsters-rolodex/static/js/2.de182e51.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Monsters-rolodex/static/js/2.de182e51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a69ef9a6fb9cba6fb119",
+    "url": "/React-Monsters-rolodex/static/js/main.8cc4dad0.chunk.js"
+  },
+  {
+    "revision": "0ced7392e0bd88409831",
+    "url": "/React-Monsters-rolodex/static/js/runtime-main.dd9e4059.js"
+  }
+]);
